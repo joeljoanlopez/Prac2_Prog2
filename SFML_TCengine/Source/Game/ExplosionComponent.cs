b79da5a71a1b1a0c53ac6c7323ee70fed2603cc1
Joloharo <joeljoanlopez@gmail.com>
2023-05-31 11:09:24 +0200

    ExplosionComponent hecho, me falta la parte 8

--- SFML_TCengine/Source/Game/ExplosionComponent.cs
+++ SFML_TCengine/Source/Game/ExplosionComponent.cs
@@ -21,15 +21,19 @@ namespace TCGame
         {
             base.OnActorDestroyed();
 
-            if( m_DisableExplosion == false)
+            if (m_DisableExplosion == false)
             {
                 // TODO (7): Create the Explosion actor add it to the scene. This actor has the next components:
                 //  - TransformComponent
                 //  - AnimatedSpriteComponent (you can Use the Explosion texture from the FX folder)
                 //  - TimeToDieComponent
                 //  - ForwardMovementComponent (optional) -> You can add it if you want, to add a very subtle movement
-
-                
+                Actor _Explosion = new Actor("Explosion");
+                TransformComponent _TransformComponent = _Explosion.AddComponent<TransformComponent>();
+                AnimatedSpriteComponent _AnimatedSpriteComponent = _Explosion.AddComponent<AnimatedSpriteComponent>("Data/Textures/FX/Explosion.png", 4, 1);
+                TimeToDieComponent _TimeToDieComponent = _Explosion.AddComponent<TimeToDieComponent>();
+                ForwardMovementComponent _ForwardMovementComponent = _Explosion.AddComponent<ForwardMovementComponent>();
+                TecnoCampusEngine.Get.Scene.AddActor(_Explosion);
             }
         }
 
